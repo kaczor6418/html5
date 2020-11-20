45472cb711954a2f0f943b4ac3bb638ab1d6e37f
kaczor6418 <k.kaczynski97@gmail.com>
2020-11-20 16:27:39 +0100

    feat: draw stairs

--- wirZastepczy/wirZastepczy.js
+++ wirZastepczy/wirZastepczy.js
@@ -1,4 +1,21 @@
 (function () {
-    const stairsAndBall = document.querySelector('#stairs-and-ball');
-    const stairsAndBallContext = stairsAndBall.getContext();
+
+    const canvas = document.querySelector('#stairs-and-ball');
+    const context = canvas.getContext('2d');
+
+    const drawRectangle = (x, y, width, height) => {
+        context.beginPath();
+        context.rect(x, y, width, height);
+        context.fill();
+    }
+
+    const drawStairs = () => {
+        window.setTimeout(() => drawRectangle(100, 100, 100, 100), 1000);
+        window.setTimeout(() => drawRectangle(100, 200, 200, 100), 2000);
+        window.setTimeout(() => drawRectangle(100, 300, 300, 100), 3000);
+        window.setTimeout(() => drawRectangle(100, 400, 400, 100), 4000);
+        window.setTimeout(() => drawRectangle(100, 500, 500, 100), 5000);
+    }
+
+    window.webkitRequestAnimationFrame(drawStairs);
 })();
