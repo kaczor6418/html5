d762953cd8534cdf0db0fa5ccc8adfabe896ff8a
kaczor6418 <k.kaczynski97@gmail.com>
2020-12-18 13:21:15 +0100

    feat: add canvas service

--- wywrotka/animation.js
+++ wywrotka/animation.js
@@ -1,5 +1,10 @@
 import { crateConfig, platformConfig, truckConfig } from './config.js';
 
-console.log('Truck config:', truckConfig);
-console.log('Platform config:', platformConfig);
-console.log('Crate config:', crateConfig);
+const baseXPosition = 100;
+const baseYPosition = 100;
+const mapWidth = 1000;
+const mapHeight = 1000;
+const speed = 5;
+
+const canvas = document.createElement('canvas');;
+const context = canvas.getContext('2d');
