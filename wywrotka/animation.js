f5d54e01e8c5be40c2a17f739346485bd4e54a32
kaczor6418 <k.kaczynski97@gmail.com>
2021-01-08 12:28:49 +0100

    refactor(trolley): remove canvas elements

--- wywrotka/animation.js
+++
@@ -1,9 +0,0 @@
-import { CanvasService } from './src/CanvasService.js';
-import { crateConfig, platformConfig, truckConfig } from './config.js';
-
-const mapWidth = 1000;
-const mapHeight = 1000;
-const speed = 5;
-const canvas = new CanvasService(mapWidth, mapHeight);
-
-canvas.attach(document.body);
