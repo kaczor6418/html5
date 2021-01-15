b834db0c2f55f6371b09990e7758061bcc11385c
kaczor6418 <k.kaczynski97@gmail.com>
2021-01-15 13:43:19 +0100

    feat: add products generator

--- koszyk/koszyk.js
+++ koszyk/koszyk.js
@@ -1,2 +1,42 @@
+import products from './products.js';
+
 const shoppingList = document.querySelector('.shop-items__list');
 
+products.forEach(({photoUrl, description}) => shoppingList.appendChild(createProductItem(photoUrl, description)));
+
+function createProductItem(productPhotol, productDescription) {
+    const productWrapper = document.createElement('li');
+    const photo = createProductPhoto(productPhotol);
+    const description = createProductDescription(productDescription);
+    const button = createProductButton();
+    productWrapper.appendChild(button);
+    productWrapper.appendChild(description);
+    productWrapper.appendChild(photo);
+    return productWrapper;
+
+}
+
+function createProductButton() {
+    const button = document.createElement('butotn');
+    button.textContent = 'Add product';
+    button.addEventListener('click', () => (console.log('Run add animation')));
+    return button;
+}
+
+function createProductDescription(text) {
+    const description = document.createElement('p');
+    description.textContent = text;
+    return description;
+}
+
+function createProductPhoto(url) {
+    const photoWrapper = document.createElement('picture');
+    const source = document.createElement('source');
+    const fallback = document.createElement('img');
+    source.srcset = url;
+    fallback.src = url;
+    fallback.alt = 'Product photo';
+    photoWrapper.appendChild(fallback);
+    photoWrapper.appendChild(source);
+    return photoWrapper;
+}
---
+++ koszyk/products.js
@@ -0,0 +1,6 @@
+export default [
+    {
+        photoUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/58/Instagram_egg.jpg/220px-Instagram_egg.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend lacus viverra, tincidunt purus vestibulum, semper ante. Mauris mollis lacus.'
+    }
+];
